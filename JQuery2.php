f92d4408a05bb1a4c795dcc7fe9642b076b59584
Victor Medina <vmedina@ucol.mx>
2017-11-09 17:35:40 -0600

    Ajustes de Victor medina

--- JQuery2.php
+++ JQuery2.php
@@ -43,7 +43,7 @@
 </head>
 <body>
 	
-	<p>Esta es tu lista de Tareas!</p>
+	<p>¡Esta es tu lista de Tareas!</p>
 	<ol id="lista">
 		<li id="item_1">Estudiar JQuery</li>
 		<li id="item_2">Ir con el dentista</li>
@@ -52,17 +52,11 @@
 	</ol>
 	<button id="eli_ult">Eliminar &Uacute;ltima Tarea</button>
 	<button id="eli_pri">Eliminar Primer Tarea</button></br></br>
-
 	<p>Escribe cualquier n&uacute;mero</p>
-
 	<input type="text" size="1" id="eliminar">
 	<button id="eli_num">Eliminar n&uacute;mero</button></br></br>
-
 	<input type="text" id="añadir">
 	<button id="aña_tare">A&ntilde;adir tarea</button></br></br>
-
 	<button id="limpiar">Eliminar la lista de Tareas?</button>
-	
-
 </body>
 </html>
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # ejercicios_educ
-Mi nombre es Cirilo Batista :)
 Ejercicios de jquery y php
